4866cefd667f65e83bbddcf11d2286f338724575
Solomon Hykes <solomon@dagger.io>
2025-10-01 16:47:03 -0700

    Simplify the CI monolith
    - Remove unnecessary abstractions: "checks router", "sdk all"..
    - Use new Changeset type for Generate() and Bump()
    - Streamline use of errgroups and custom spans, with 'parallel' utility
    - 'generate' also generates github actions config and changelog
    - New convention: prefix checks with 'check-'
    - 'check-lint' runs all linters, not just core go linter
    - Remove dirdiff module (deprecated by Changeset)
    - Move non-linting checks out of lint functions
    - Simplify github actions generated config slightly
    
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ util/parallel/parallel.go
@@ -0,0 +1,68 @@
+package parallel
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+	"golang.org/x/sync/errgroup"
+)
+
+func New() parallelJobs {
+	return parallelJobs{
+		Jobs: map[string]JobFunc{},
+	}
+}
+
+func Run(ctx context.Context, name string, fn JobFunc) error {
+	return New().WithJob(name, fn).Run(ctx)
+}
+
+type parallelJobs struct {
+	Jobs map[string]JobFunc
+}
+
+type JobFunc func(context.Context) error
+
+func (p parallelJobs) WithJob(name string, fn JobFunc) parallelJobs {
+	clone := New()
+	for k, v := range p.Jobs {
+		clone.Jobs[k] = v
+	}
+	clone.Jobs[name] = fn
+	return clone
+}
+
+func (p parallelJobs) Run(ctx context.Context) error {
+	var eg errgroup.Group
+	for name, job := range p.Jobs {
+		name, job := name, job
+		eg.Go(func() error {
+			opts := []trace.SpanStartOption{
+				trace.WithAttributes(attribute.Bool("dagger.io/ui.reveal", true)),
+			}
+			ctx, span := otel.Tracer("dagger.io/util/parallel").Start(ctx, name, opts...)
+			var err error
+			if job != nil {
+				err = job(ctx)
+			}
+			if err != nil {
+				span.SetStatus(codes.Error, err.Error())
+			}
+			span.End()
+			return err
+		})
+	}
+	return eg.Wait()
+}
+
+func (p parallelJobs) RunSerial(ctx context.Context) error {
+	for name, job := range p.Jobs {
+		if err := Run(ctx, name, job); err != nil {
+			return err
+		}
+	}
+	return nil
+}
