6438b9c388b8e858e6b31ab63031c8173893794f
Solomon Hykes <solomon@dagger.io>
2025-11-03 09:21:32 -0800

    wip

---
+++ toolchains/trivy/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"dagger/security/internal/dagger"
+
+	"github.com/dagger/dagger/.dagger/internal/dagger"
+)
+
+type Trivy struct {
+	Source      *dagger.Directory
+	ConfigFiles *dagger.Directory
+}
+
+func New(
+	// The source tree to run the security toolchain against
+	// +defaultPath="/"
+	source *dagger.Directory,
+
+	// A directory containing trivvy scan config files.
+	// +defaultPath="/"
+	// +ignore=["*", "!.trivyignore", "!.trivyignore.yml", "!.trivyignore.yaml"]
+	configFiles *dagger.Directory,
+) *Trivy {
+	return &Trivy{
+		Source:      source,
+		ConfigFiles: configFiles,
+	}
+}
+
+// Scan source code for security vulnerabilities
+// +cache="session"
+func (trivy *Trivy) ScanSource(
+	ctx context.Context,
+	// +optional
+	exclude []string,
+) (MyCheckStatus, error) {
+	src := trivy.Source
+	if len(exclude) > 0 {
+		src = trivy.Source.Filter(dagger.DirectoryFilterOpts{
+			Exclude: exclude,
+		})
+	}
+	_, err := dag.Container().
+		From("aquasec/trivy:0.67.2@sha256:e2b22eac59c02003d8749f5b8d9bd073b62e30fefaef5b7c8371204e0a4b0c08").
+		WithMountedCache("/root/.cache/", dag.CacheVolume("trivy-cache")).
+		WithMountedDirectory("/src", src).
+		WithExec([]string{
+			"trivy", "fs", "--pkg-types=library", "--scanners=vuln",
+			"--format=json",
+			"--exit-code=1",
+			"--severity=CRITICAL,HIGH",
+			"--show-suppressed",
+		}).
+		Sync(ctx)
+	return CheckCompleted, err
+}
+
+// Scan container images for security vulnerabilities
+func (trivy *Trivyscan) ScanContainers(ctx context.Context) (MyCheckStatus, error) {
+
+
+		args := []string{
+			"trivy",
+			"image",
+			"--pkg-types=os,library",
+		}
+		args = append(args, commonArgs...)
+		engineTarball := "/mnt/engine.tar"
+		args = append(args, "--input", engineTarball)
+
+		target := dag.DaggerEngine().Container()
+		_, err = ctr.
+			WithMountedFile(engineTarball, target.AsTarball()).
+			WithExec(args).
+			Sync(ctx)
+		return err
+	}).
+		Run(ctx)
+}
